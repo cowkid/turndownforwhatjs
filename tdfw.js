8a55451b26608a0a2e1987b34d88ddb0a0540f57
Cow Kid <cowkid33@gmail.com>
2017-06-14 19:39:59 -0400

    Create tdfw.js

--- tdfw.js
+++ tdfw.js
@@ -67,7 +67,7 @@
 			player = new YT.Player('tdfw', {
 				height: '200',
 				width: '305',
-				videoId: 'HMUDVMiITOU',
+				videoId: 'U9t-slLl30E',
 				events: {
 					'onReady': onPlayerReady,
 					'onStateChange': onPlayerStateChange
